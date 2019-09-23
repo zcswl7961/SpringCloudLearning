8b2c29e6b6d8151c6d6cb978145cdba55948252f
chenggong zhou <26921832@qq.com>
2019-09-23 09:14:13 +0800

    实例编写

--- sc-f-chapter3/service-feign/src/main/java/com/forezp/servicefeign/ServiceFeignApplication.java
+++ sc-f-chapter3/service-feign/src/main/java/com/forezp/servicefeign/ServiceFeignApplication.java
@@ -3,11 +3,9 @@ package com.forezp.servicefeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ServiceFeignApplication {
--- sc-f-chapter3/service-feign/src/main/java/com/forezp/servicefeign/clients/SchedualServiceHi.java
+++ sc-f-chapter3/service-feign/src/main/java/com/forezp/servicefeign/clients/SchedualServiceHi.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
+ *
+ * 1，首先使用Feign的时候，定义一个接口，接口的定义一个注解@FeignClient(),指名对应的调用服务
+ * 2，定义一个方法，指定对应的方法的调用参数数据信息和对应的需要调用的服务保持一致
  * Email miles02@163.com
  *
  * @author fangzhipeng
