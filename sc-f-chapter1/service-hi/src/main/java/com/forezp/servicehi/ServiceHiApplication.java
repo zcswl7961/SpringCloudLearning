11c5a37beb10c7ca7ff503204e864f4da114bab8
chenggong zhou <26921832@qq.com>
2019-10-02 18:27:25 +0800

    eureka 配置注解

--- sc-f-chapter1/service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
+++ sc-f-chapter1/service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +19,7 @@ public class ServiceHiApplication {
 
     @Value("${server.port}")
     String port;
+
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
